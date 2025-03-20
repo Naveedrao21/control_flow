c65bdbc93947c58288b322e72d7a797ec01893d4
Rao Naveed <oyerao18@gmail.com>
2025-03-20 21:38:21 +0500

    Add conditional assignment and print statement in main.rs

--- src/main.rs
+++ src/main.rs
@@ -22,5 +22,11 @@ fn main() {
     }
      else {
         println!("number is not divisible by 4, 3, or 2");
+        let condition = false;
+        let number = if condition { 5 } else { 6 };
+    
+        println!("The value of number is: {number}");
+
+
     }
 }
