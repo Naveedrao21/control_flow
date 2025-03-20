6d0f44060284ecacfa18d1b2f193de6a2266b908
Rao Naveed <oyerao18@gmail.com>
2025-03-20 21:26:58 +0500

    Enhance main.rs with if-else and if-else if expressions for number divisibility checks

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,26 @@
 fn main() {
     // if expression
-    let number = 3;
-    if number < 7 {
+    let number = 0;
+    if number != 0 {
         println! ("i am naveed" );
     }
     else {
         println! ("i am not naveed");
+    }
+
+    // if else if expression
+    let number = 7;
+
+    if number % 4 == 0 {
+        println!("number is divisible by 4");
+    } 
+    else if number % 3 == 0 {
+        println!("number is divisible by 3");
+    } 
+    else if number % 2 == 0 {
+        println!("number is divisible by 2");
+    }
+     else {
+        println!("number is not divisible by 4, 3, or 2");
     }
 }
